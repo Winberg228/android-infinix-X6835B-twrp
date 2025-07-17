ea7ec786baf630effc33c84d60a2ba51d686790f
KSN <redawewrq12@gmail.com>
2025-07-17 22:52:40 +0300

    X6835B : FIX BOOTLOOP?

--- device.mk
+++ device.mk
@@ -6,19 +6,27 @@
 #
 
 LOCAL_PATH := device/infinix/X6835B
-
-# Dynamic partitions
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
+# A/B
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl-1.2-mtkimpl \
-    
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl-2.1 \
-    android.hardware.health@2.0-impl-default \
-    libhealthd.$(PRODUCT_PLATFORM)
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-impl.recovery \
+    bootctrl.mt6765 \
+    bootctrl.mt6765.recovery \
+    android.hardware.boot@1.2-service
 
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mtk \
-    fastbootd
+    bootctrl.mt6765
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
